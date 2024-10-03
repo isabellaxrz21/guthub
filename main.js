ae6dd261b2199a4266dfc2a1ae2b76005c1803f0
isabellaxrz21 <163482890+isabellaxrz21@users.noreply.github.com>
2024-10-03 12:18:07 -0300

    kjcbakjd

---
+++ main.js
@@ -0,0 +1,18 @@
+let países=['BRASIL','USA','RUSSIA','ALEMANHA','BELGICA','EGITO','CANADA','AUSTRALIA','INGLATERRA'];
+undefined
+países.length
+9
+países[1]='MEXICO'
+'MEXICO'
+let i=0;
+    while (i<) {
+        console.log(i);
+    }
+let i=0;
+    
+while (i<países.length) {
+        console.log(i);
+        console.log(países[i]);                                             
+    i++;                                                                                                                                          }
+
+    
